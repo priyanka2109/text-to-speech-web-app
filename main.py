c0f0575febc643150d2411a284e7549697eab23b
priyanka2109 <57538743+priyanka2109@users.noreply.github.com>
2020-06-04 20:41:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
-app = Flask(__name__,static_folder='static')
+from flask_nav import Nav
+from flask_nav.elements import Navbar, Subgroup, View, Link, Text, Separator
+app = Flask(__name__, static_folder='static')
+
+
 
 @app.route("/")
 def home():
@@ -7,6 +11,11 @@ def home():
 @app.route("/about")
 def about():
     return render_template("About.html")
+
+@app.route("/Contact")
+def contact():
+    return render_template("Contact.html")
+
 
 if __name__ == "__main__":
     app.run()
